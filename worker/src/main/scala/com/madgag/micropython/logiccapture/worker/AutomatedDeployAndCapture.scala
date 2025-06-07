feb98adcc428edf803e2b5d07acc9637db1ecbd5
Roberto Tyley <roberto.tyley@gmail.com>
2025-06-07 11:02:11 +0100

    Probably will not execute

--- worker/src/main/scala/com/madgag/micropython/logiccapture/worker/AutomatedDeployAndCapture.scala
+++ worker/src/main/scala/com/madgag/micropython/logiccapture/worker/AutomatedDeployAndCapture.scala
@@ -1,9 +1,11 @@
 package com.madgag.micropython.logiccapture.worker
 
+import cats.effect.{IO, Resource}
 import org.virtuslab.yaml.*
 import os.*
 
 object AutomatedDeployAndCapture {
+
   def process(workRoot: Path, captureConfigFileSubPath: SubPath, resultsDir: Path): Unit = {
     println(s"path is ${sys.env("PATH")}")
     val captureConfigFile = workRoot / captureConfigFileSubPath
@@ -27,20 +29,23 @@ object AutomatedDeployAndCapture {
 
       val captureResultsFile = resultsDir / "capture.csv"
 
-      val mpremoteProcess = os.proc(
-        "mpremote",
-        "connect", "id:560ca184b37d9ae2",
-        "mount", workRoot / mountFolder,
-        "exec", s"import ${cap.startImport}"
-      ).spawn(stdin = os.Inherit, stdout = os.Inherit, stderr = os.Inherit)
-
-      val captureProcess =
-        os.proc("TerminalCapture", "capture", "/dev/ttyACM0", workRoot / captureDef, captureResultsFile).spawn()
-
-      captureProcess.waitFor(10000)
-      mpremoteProcess.destroy()
-      println(os.size(captureResultsFile))
+      for {
+        mpremoteProcess <- Resource.fromAutoCloseable(IO(connectMPRemote(workRoot, mountFolder, cap)))
+        captureProcess <- Resource.fromAutoCloseable(IO(os.proc("TerminalCapture", "capture", "/dev/ttyACM0", workRoot / captureDef, captureResultsFile).spawn()))
+      } yield {
+        captureProcess.waitFor(10000)
+        println(os.size(captureResultsFile))
+      }
     }
+  }
+
+  private def connectMPRemote(workRoot: Path, mountFolder: SubPath, cap: CaptureConfig) = {
+    os.proc(
+      "mpremote",
+      "connect", "id:560ca184b37d9ae2",
+      "mount", workRoot / mountFolder,
+      "exec", s"import ${cap.startImport}"
+    ).spawn(stdin = os.Inherit, stdout = os.Inherit, stderr = os.Inherit)
   }
 
   given YamlCodec[os.RelPath] = YamlCodec.make[String].mapInvariant(os.RelPath(_))(_.toString)
