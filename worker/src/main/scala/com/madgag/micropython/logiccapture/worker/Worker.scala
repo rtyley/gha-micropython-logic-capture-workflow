9c7a538b01767c47ac31c1679f6c843048cbfe5d
Roberto Tyley <roberto.tyley@gmail.com>
2025-06-09 19:28:14 +0100

    Org imports

--- worker/src/main/scala/com/madgag/micropython/logiccapture/worker/Worker.scala
+++ worker/src/main/scala/com/madgag/micropython/logiccapture/worker/Worker.scala
@@ -3,12 +3,8 @@ package com.madgag.micropython.logiccapture.worker
 import cats.*
 import cats.effect.{IO, IOApp}
 import com.madgag.micropython.logiccapture.worker.LogicCaptureWorker.JobDef
-import com.madgag.micropython.logiccapture.worker.aws.StepFuncClient.{GetTaskResponse, Token}
-import com.madgag.micropython.logiccapture.worker.aws.{AWS, Fail, StepFuncActivityClient}
-import software.amazon.awssdk.awscore.AwsResponse
-import software.amazon.awssdk.core.SdkPojo
-import software.amazon.awssdk.services.sfn.model.{SendTaskFailureResponse, SendTaskSuccessResponse, SfnResponse}
-import software.amazon.awssdk.utils.builder.{CopyableBuilder, ToCopyableBuilder}
+import com.madgag.micropython.logiccapture.worker.aws.StepFuncClient.GetTaskResponse
+import com.madgag.micropython.logiccapture.worker.aws.{AWS, StepFuncActivityClient}
 import upickle.default.*
 
 object Worker extends IOApp.Simple {
@@ -20,7 +16,7 @@ object Worker extends IOApp.Simple {
   override val run: IO[Unit] = (for {
     captureTaskOpt <- activityClient.fetchTaskIfAvailable("pico-logic-capture")
   } yield captureTaskOpt.fold(IO(()), handleTask)).foreverM
-  
+
   def handleTask(task: GetTaskResponse): IO[Unit] = for {
     result <- activityWorker.process(read[JobDef](task.input), () => activityClient.sendHeartbeat(task.token))
   } yield result.fold(
