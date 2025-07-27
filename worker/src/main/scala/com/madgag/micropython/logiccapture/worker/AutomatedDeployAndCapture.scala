2bf20e9671410d9b08bd86daf02799ac4e1f669c
Roberto Tyley <roberto.tyley@gmail.com>
2025-07-27 16:10:29 +0100

    We've been seeing java.io.IOException: Stream closed

--- worker/src/main/scala/com/madgag/micropython/logiccapture/worker/AutomatedDeployAndCapture.scala
+++ worker/src/main/scala/com/madgag/micropython/logiccapture/worker/AutomatedDeployAndCapture.scala
@@ -71,7 +71,7 @@ object AutomatedDeployAndCapture {
       mpremoteProcess <- Resource.fromAutoCloseable(IO(connectMPRemote(workRoot, mountFolder, cap)))
       captureProcess <- Resource.fromAutoCloseable(IO(connectCapture(gusmanbConfigFile, captureResultsFile)))
     } yield (mpremoteProcess, captureProcess)).use { case (mpremoteProcess, captureProcess) =>
-      IO.blocking(captureProcess.join(10000)) >> IO {
+      IO.blocking(captureProcess.waitFor(10000)) >> IO {
         CaptureResult(captureProcess.stdout.trim(), compactCapture(captureResultsFile, gusmanbConfig))
       }
     }
